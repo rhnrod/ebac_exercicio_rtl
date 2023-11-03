cca188964b88f1a376a029e7a12907fa916048d4
Rhuan Emanuel <rhnrod@gmail.com>
2023-11-02 23:33:26 -0300

    exercício respondido

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,5 +1,6 @@
+// <reference= />
+
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
@@ -7,4 +8,47 @@ describe('Teste para o componente PostComment', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
+
+    it('Deve adicionar "Olá, Jest" na lista', () => {
+        render(<PostComment/>);
+        fireEvent.change(screen.getByTestId('textarea-tarefa'), {
+            target: {
+                value: 'Estudar Jest'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-cadastrar'))
+        expect(screen.getByText('Estudar Jest')).toBeInTheDocument()
+    })
+
+    it('Deve adicionar "Olá, testes" na lista', () => {
+        render(<PostComment/>);
+        fireEvent.change(screen.getByTestId('textarea-tarefa'), {
+            target: {
+                value: 'Olá, testes'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-cadastrar'))
+        expect(screen.getByText('Olá, testes')).toBeInTheDocument()
+    })
+
+    it('Deve haver dois comentários na lista', () => {
+        render(<PostComment />);
+        fireEvent.change(screen.getByTestId('textarea-tarefa'), {
+            target: {
+                value: 'Estudar Jest'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-cadastrar'))
+
+        fireEvent.change(screen.getByTestId('textarea-tarefa'), {
+            target: {
+                value: 'Olá, testes'
+            }
+        })
+        fireEvent.click(screen.getByTestId('btn-cadastrar'))
+
+        const comments = screen.getAllByTestId('li-comment')
+        expect(comments).toHaveLength(2);
+        
+    })
 });
